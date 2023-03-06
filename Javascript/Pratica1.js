c252e633aef013a6d7c643449aa47920a61d066b
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-03-06 19:19:28 +0000

    primeiro exemplo

---
+++ Javascript/Pratica1.js
@@ -0,0 +1,11 @@
+print (1, 2, 3);
+
+print(6, "Teste");
+ //Imprimir uma string, um núm. e outra string
+ print("Bom dia",2,"Tchau");
+
+ print(6, "Teste");
+ //Imprimir uma string, um núm. e outra string
+ print("Bom dia",2,"print");
+
+ 
